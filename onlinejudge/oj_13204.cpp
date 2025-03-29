f1fb755bf8e2523fcc186be27da50cc34026e409
yuto0226 <wayne71112@gmail.com>
2025-03-29 16:11:31 +0800

    Solve uva 13204

---
+++ onlinejudge/oj_13204.cpp
@@ -0,0 +1,25 @@
+// proof https://oeis.org/A062870
+#include <iostream>
+#include <vector>
+using namespace std;
+
+#define MOD 1000000007
+#define INPUT_MAX 1000001
+
+vector<long long> factorial(INPUT_MAX, 1);
+
+int main() {
+  for (int i = 1; i < INPUT_MAX; i++) {
+    factorial[i] = (factorial[i - 1] * i) % MOD;
+  }
+
+  int n;
+  while (cin >> n) {
+    long long result = factorial[n / 2] * factorial[n / 2] % MOD;
+    if (n % 2) {
+      result = n * result % MOD;
+    }
+  }
+
+  return 0;
+}
